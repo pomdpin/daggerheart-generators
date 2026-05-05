0b65dec497ea52273d59f5c356a499b71ffde2d1
pomdpin <pomdpin@gmail.com>
2026-05-05 22:45:00 +0000

    again

--- pages/2 Loot Generator.py
+++ pages/2 Loot Generator.py
@@ -29,28 +29,31 @@ nbr_object = st.number_input("How many items do you want ?", min_value = 1, valu
 
 col1,col2 = st.columns(2)
 
-with col2:
-    if st.button("Open loot database"):
-        st.dataframe(list_loot, use_container_width=True)
-
 with col1:
-    if st.button("Generate Loot") :
-        loot_list=[]
-        for i in range(nbr_object):
-            if options_object == "All" and options_tiers == "All":
-                loot = list_loot.sample(ignore_index=True)
-                
-            else:
-                loot_by_type = list_loot[list_loot["Type"].str.contains('|'.join(selec_object))]
-                loot_by_tier = loot_by_type[loot_by_type["Rarity"].str.contains('|'.join(selec_tiers))]
-                loot = loot_by_tier.sample(ignore_index=True)
-            
-            loot = loot.to_dict()
-            loot = {key: list(value.values())[0] for key, value in loot.items()}
-            loot_list.append(loot)
+    generate = st.button("Generate loot")
+with col2:
+    database = st.button("Open loot database")
 
-        cols_per_row = 2
-        cols = st.columns(cols_per_row)
+if database:
+    st.dataframe(list_loot, use_container_width=True)
+
+if generate :
+    loot_list=[]
+    for i in range(nbr_object):
+        if options_object == "All" and options_tiers == "All":
+            loot = list_loot.sample(ignore_index=True)
+            
+        else:
+            loot_by_type = list_loot[list_loot["Type"].str.contains('|'.join(selec_object))]
+            loot_by_tier = loot_by_type[loot_by_type["Rarity"].str.contains('|'.join(selec_tiers))]
+            loot = loot_by_tier.sample(ignore_index=True)
+        
+        loot = loot.to_dict()
+        loot = {key: list(value.values())[0] for key, value in loot.items()}
+        loot_list.append(loot)
+
+    cols_per_row = 2
+    cols = st.columns(cols_per_row)
 
     for i, loot in enumerate(loot_list):
         with cols[i % cols_per_row]:
