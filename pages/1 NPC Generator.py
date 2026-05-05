f7bb3190dcd291caeb768f4276bd2dccfb5613e9
pomdpin <pomdpin@gmail.com>
2026-05-05 23:13:45 +0000

    modify output

--- pages/1 NPC Generator.py
+++ pages/1 NPC Generator.py
@@ -79,10 +79,10 @@ if create:
         if on:
             pnj_name = f"""{name} {surname} - Tier {tier} {community} {ascendance_random} {classe}"""
             pnj_desc = f"""
-            \r**Description** : {age} {age_terme}. {phrase_descr_asc} with {descr_general[0]} eyes,  wearing {descr_general[1]} {descr_general[2]}.
-            \rQuirk : {descr_general[3]}
-            \rPossible Occupation : 
-            \rHome :"""
+            \r**Description**: {age} {age_terme}. {phrase_descr_asc} with {descr_general[0]} eyes,  wearing {descr_general[1]} {descr_general[2]}.
+            \r**Quirk**: {descr_general[3]}
+            \r**Occupation**: 
+            \r**Home**:"""
         else :
             pnj_name = f"{name} {surname}"
             pnj_desc = f"""{classe} faisant partie de la {community},
